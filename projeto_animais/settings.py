1aceda12ca93acc1f93ea3e932396452f20a62c3
Gleiciane Silva <71572039+GleicianeSilva@users.noreply.github.com>
2023-12-10 23:42:45 -0300

    Update settings.py

--- projeto_animais/settings.py
+++ projeto_animais/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h97q0e-&_22vx79b3edu&bz8$7vzte)8rk)yg&n8p^vzq4%&bd"
+# SECRET_KEY = "django-insecure-h97q0e-&_22vx79b3edu&bz8$7vzte)8rk)yg&n8p^vzq4%&bd"
+
+SECRET_KEY = "be0ee58105bde6969344a56f2a52d3023d2b62d4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
